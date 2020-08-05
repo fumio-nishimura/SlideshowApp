996bd5fd6a420119000a69764ea88c64ef059329
fumio-nishimura <cityhunter0522@gmail.com>
2020-08-05 17:10:32 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 西村史旺 on 2020/08/05.
+//  Copyright © 2020 Fumio Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
